51b347b2eb03a765b409780572acc9e9e7f8bc78
AbdurRahim12-dev <rahim.intern@excelbd.com>
2024-12-30 19:09:35 +0600

    Enhance Press Release component with dynamic word animation

--- src/components/Home/Press_Release.jsx
+++ src/components/Home/Press_Release.jsx
@@ -1,7 +1,11 @@
 import { motion } from 'motion/react';
 
 const Press_Release = () => {
-  
+  const text =
+    ' At Ardentware, we empower businesses t stay ahead in a rapidly evolving world. By modernizing technology, reimagining processes, and transforming experiences, we let you focus on your business while we seamlessly manage the technology that drives it.';
+  // const line = text.split('.');
+  const words = text.split(' ');
+
   return (
     <>
       <div className='bg-accent text-white'>
@@ -20,8 +24,8 @@ const Press_Release = () => {
           </motion.h2>
         </div>
       </div>
-      <div className="container py-16">
-      <motion.h4
+      <div className='container py-16'>
+        {/* <motion.h4
         style={{
           fontWeight: 300,
           color: 'rgba(0, 0, 72, 1)',
@@ -37,8 +41,38 @@ const Press_Release = () => {
         <span className="font-bold text-accent">reimagining</span> processes, and transforming
         experiences, we let you focus on your business while we seamlessly manage the technology
         that drives it.
-      </motion.h4>
-    </div>
+      </motion.h4> */}
+
+        <motion.h3
+          className='text-2xl md:text-[1.8rem] lg:text-[36px] text-left text-wrap leading-tight'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+        >
+          {words.map((word, index) => (
+            <motion.span
+              key={index}
+              className='inline-block text-2xl md:text-[1.8rem] lg:text-[36px] text-left text-wrap leading-tight  '
+              style={{
+                fontWeight: 300,
+                color: 'rgba(0, 0, 72, 1)',
+              }}
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ amount: 0.8, once: true }}
+              transition={{
+                delay: index * 0.1, // Add delay for each word
+                type: 'spring',
+                stiffness: 100,
+                damping: 20,
+              }}
+              // dangerouslySetInnerHTML={{ __html: word }}
+            >
+              {word}&nbsp;
+            </motion.span>
+          ))}
+        </motion.h3>
+      </div>
     </>
   );
 };
